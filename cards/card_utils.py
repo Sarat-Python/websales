a9faa46451b31cbda26d9851d4a31cd001444138
Prashanth <prashanth@pitstoprecharge.com>
2014-01-30 20:58:29 +1100

    Added copyright info

--- cards/card_utils.py
+++ cards/card_utils.py
@@ -1,3 +1,26 @@
+'''
+PIT_STOP_RECHARGE_BEGIN_TAG
+*
+* Pit Stop Recharge CONFIDENTIAL
+*
+* COPYRIGHT Pit Stop Recharge P/L 2011, 2014
+* All Rights Reserved
+*
+* The source code for this program is not published or otherwise 
+* divested of its trade secrets, irrespective of what has been 
+* deposited with the Australian Copyright Office. 
+*
+PIT_STOP_RECHARGE_END_TAG
+'''
+'''
+Begin Change Log ***************************************************************
+                                                                      
+  Itr    Def/Req  Userid      Date       Description
+  -----  -------- --------    --------   --------------------------------------
+  0.9    339      prashanth  19/01/2014  Added copyright Info
+ End Change Log ****************************************************************
+'''
+
 #from cards.models import generated_batch, bulk_cards
 from card_info import CARD_TYPES
 
@@ -19,7 +42,8 @@ def get_blackhawk_upc_code(card_number):
 
 
 def get_woolworth_upc_code(card_number):
-    # First 9 digits of a card is a batch number. +6280003090916632185=271050212593?
+    # First 9 digits of a card is a batch number. 
+    # +6280003090916632185=271050212593?
     upc_code = card_number[:9]
     return upc_code
 
--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,26 @@
+'''
+PIT_STOP_RECHARGE_BEGIN_TAG
+*
+* Pit Stop Recharge CONFIDENTIAL
+*
+* COPYRIGHT Pit Stop Recharge P/L 2011, 2014
+* All Rights Reserved
+*
+* The source code for this program is not published or otherwise 
+* divested of its trade secrets, irrespective of what has been 
+* deposited with the Australian Copyright Office. 
+*
+PIT_STOP_RECHARGE_END_TAG
+'''
+'''
+Begin Change Log ***************************************************************
+                                                                      
+  Itr    Def/Req  Userid      Date       Description
+  -----  -------- --------    --------   --------------------------------------
+  0.9    339      prashanth  19/01/2014  Added copyright Info
+ End Change Log ****************************************************************
+'''
+
 # Create your views here.
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
