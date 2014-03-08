2096829a94ab959f56b538953f8492e67f5cba07
PitStop <srinu@ubuntu.(none)>
2014-03-08 12:51:54 +0530

    user stories version 1

--- cards/urls.py
+++ cards/urls.py
@@ -24,20 +24,16 @@ Begin Change Log ***************************************************************
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'webshop.views.home', name='home'),
-    # url(r'^webshop/', include('webshop.foo.urls')),
-    
-    #url(r'^$', 'users.views.login'),
-    
+   
     # For bulk card activation
+    url(r'^bulk/load_flavours/$', 'cards.views.load_flavours'),
     url(r'^bulk/purchase/$', 'cards.views.bulk'),
     url(r'^bulk/update/$', 'cards.views.update'),
     url(r'^bulk/add_cart/$', 'cards.views.add_cart'),
     url(r'^bulk/del_cart/$', 'cards.views.del_cart'),
-    #url(r'^bulk/edit/$', 'cards.views.bulk_edit'),
-    #url(r'^bulk/update/$', 'cards.views.bulk_update'),
     url(r'^purchase/$', 'cards.views.purchase'),
-    #url(r'^activate/batch/$', 'cards.views.activate_batch'),
+
    
 )
+
+
