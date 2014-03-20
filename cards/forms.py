135e0117d823a20ebecfc5885d4a0dbda52c3c9d
NaveeN <naveen.d@hexagonglobal.in>
2014-03-20 16:28:13 +0530

    Prepared Code formatting

--- cards/forms.py
+++ cards/forms.py
@@ -12,13 +12,14 @@ PIT_STOP_RECHARGE_BEGIN_TAG
 *
 PIT_STOP_RECHARGE_END_TAG
 '''
+
 '''
-Begin Change Log **************************************************************************
+Begin Change Log *************************************************************
                                                                       
-  Itr        Def/Req               Userid      Date           Description
-  -----     --------               --------  --------       -------------------------------
-  Sprint     Bug 8,9,10,11,12      NaveeN    19/03/2014       Added Shopcart Functionality
- End Change Log ***************************************************************************
+  Itr        Def/Req          Userid      Date           Description
+  -----     --------          --------  --------       ------------------
+  Sprint     Bug 8,9,10,11,12 NaveeN    20/03/2014     Added Code formatting
+ End Change Log **************************************************************
 '''
 
 from django import forms
@@ -35,50 +36,60 @@ from cards.card_info import CARD_TYPES
 from cards.card_info import CARD_FLAVOUR
 
 
-
 class SwipedCardForm(ModelForm):
     card_type = forms.ChoiceField(choices=CARD_TYPES)
     card_flavour = forms.ChoiceField(choices=CARD_FLAVOUR)
     
     def __init__(self, *args, **kwargs):
         super(SwipedCardForm, self).__init__(*args, **kwargs)       
-        self.fields['amount'].widget.attrs = {'class':'styled validate[required,custom[number]]','size':5,'maxlength':5}
-        self.fields['card_number'].widget.attrs = {'class':'styled validate[required]','size':40}
+        self.fields['amount'].widget.attrs = {'class':
+                             'styled validate[required,custom[number]]'
+                             ,'size':5,'maxlength':5}
+        self.fields['card_number'].widget.attrs = {'class':
+                     'styled validate[required]','size':40}
    
     class Meta:
         model = SwipedCard
         fields = ['card_type','card_flavour','amount','card_number']
 
 UpdateSetBase = modelformset_factory(
-    SwipedCard, extra=0, fields=('card_type','card_flavour','amount','card_number', 'upc_code'))
+    SwipedCard, extra=0, fields=('card_type','card_flavour',
+                                 'amount','card_number', 'upc_code'))
+
 
 class UpdateFormSet(UpdateSetBase):
     
     def add_fields(self, form, index):
         super(UpdateFormSet, self).add_fields(form, index)
         form.fields['card_number'].widget.attrs['readonly']  = True
-        form.fields['upc_code'] = forms.CharField(label='UPC / Batch Code', required = True)
-        form.fields['card_type'].queryset = forms.ModelChoiceField(queryset=SwipedCard.objects.all()) 
-        form.fields['card_flavour'].queryset = forms.ModelChoiceField(queryset=SwipedCard.objects.all())
+        form.fields['upc_code'] = forms.CharField(
+                                 label='UPC / Batch Code', required = True)
+        form.fields['card_type'].queryset = forms.ModelChoiceField(
+                                 queryset=SwipedCard.objects.all()) 
+        form.fields['card_flavour'].queryset = forms.ModelChoiceField(
+                                 queryset=SwipedCard.objects.all())
         form.fields['card_flavour'].widget.attrs['disabled']  = True
         form.fields['card_type'].widget.attrs['disabled']  = True
         form.fields['upc_code'].widget.attrs['readonly']  = True
         form.fields['amount'] = forms.CharField(required=True)
-                 
+        form.fields['amount'].widget.attrs = {
+            'class':'styled form-control validate[required,custom[number]]'
+                        ,'size':5,'maxlength':5}
 
-   
+
 class UpdateSwipedCardForm(forms.Form):
     card_type = forms.ChoiceField(choices=CARD_TYPES)
-    card_flavour =  forms.ModelChoiceField(queryset=gift_cards.objects.all())      
+    card_flavour =  forms.ModelChoiceField(queryset=gift_cards.objects.all())     
     
     def __init__(self, *args, **kwargs):
         super(UpdateSwipedCardForm, self).__init__(*args, **kwargs)                   
         #self.fields['card_number'].widget.attrs['readonly']  = True
-        self.fields['card_flavour'].queryset = gift_cards.objects.filter(id=121)    
-    
+        self.fields['card_flavour'].queryset = gift_cards.objects.filter(id=121)  
+        
     class Meta:
         model = SwipedCard
         fields = ['card_type','card_flavour','amount','card_number', 'upc_code']
+   
         
     #item_field = forms.ModelChoiceField(queryset=Item.objects.none())
     #def verify_card_length(self):
@@ -103,11 +114,13 @@ class UpdateSwipedCardForm(forms.Form):
     #        if card_type in CARD_TYPES[0]:
     #            print 'splitting for woolworths'
     #            self['card_number'] = card_utils.woolworth_card_number(card)
-    #            self['upc_code']= card_utils.get_woolworth_upc_code(self['card_number'])
+    #            self['upc_code']= card_utils.get_woolworth_upc_code(
+    #                    self['card_number'])
     #        elif card_type in CARD_TYPES[1]:
     #            # for blackhawk
     #            self['card_number']= card_utils.blackhawk_card_number(card)
-    #            self['upc_code'] = card_utils.get_blackhawk_upc_code(self['card_number'])
+    #            self['upc_code'] = card_utils.get_blackhawk_upc_code(
+    #                   self['card_number'])
     #        else:
     #            print 'card type not found'
     #            return False
@@ -117,8 +130,10 @@ class UpdateSwipedCardForm(forms.Form):
 #    """
 #    A form that generates batch numbers per given range.
 #    """
-#    batch_number = forms.IntegerField(label=_('Initial batch number'),  required = True)
-#    num_cards = forms.IntegerField(label=_('Number of cards'), required = True)
+#    batch_number = forms.IntegerField(label=_('Initial batch number'), 
+#                                                   required = True)
+#    num_cards = forms.IntegerField(label=_('Number of cards'),
+#                                     required = True)
 #    cost = forms.IntegerField(label=_('Cost'), required = False)
 #    # to be mapped with existing crm vendor list
 #    vendors = (
@@ -149,7 +164,9 @@ class UpdateSwipedCardForm(forms.Form):
 #            for i, selected in enumerate(extra):
 #                card = bulk_cards.objects.get(pk=selected)
 #                print card.cost
-#                self.fields['c_%s' % selected] = forms.CharField(label=card.card_number,widget=forms.TextInput(attrs={'placeholder': card.cost}))
+#                self.fields['c_%s' % selected] = forms.CharField(
+#        label=card.card_number,widget=forms.TextInput(
+#            attrs={'placeholder': card.cost}))
 #        
 #
 #
