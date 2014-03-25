6db7d7016e9918309a312a28f5f06e27330dcfbe
NaveeN <naveen.d@hexagonglobal.in>
2014-03-25 16:22:06 +0530

    Changed Static Folder Path

--- webshop/settings.py
+++ webshop/settings.py
@@ -17,7 +17,7 @@ Begin Change Log ***************************************************************
                                                                       
   Itr    Def/Req  Userid      Date       Description
   -----  -------- --------    --------   --------------------------------------
-  0.9    339      prashanth  19/01/2014  Added copyright Info
+  0.9    339      prashanth  25/03/2014  Changed Static Folder Path
  End Change Log ****************************************************************
 '''
 
@@ -99,7 +99,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/pitstop/websales/static',
+    '/home/pitstop/websales/assets/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -220,3 +220,4 @@ LOGGING = {
         },
     }
 }
+
