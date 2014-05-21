3c1b9830452fc72c37d1de24a0ebdadf4807fd42
NaveeN <naveen.d@hexagonglobal.in>
2014-05-21 22:44:58 +0530

    modified pitstop database name

--- webshop/settings.py
+++ webshop/settings.py
@@ -38,10 +38,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'webshop',                      # Or path to database file if using sqlite3.
+        'NAME': 'pit_stop_2',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'toor',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
